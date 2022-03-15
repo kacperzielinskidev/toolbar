f7a50e342b5b8231c03b7982f393636f865e9503
Kacper Zieliński <86601708+Gasparuss@users.noreply.github.com>
2022-03-15 15:51:09 +0100

    Create showAlertMessage function

--- widget.js
+++ widget.js
@@ -1,6 +1,7 @@
-const showBarButton = document.querySelector(".showbar__button");
-const closeButton = document.querySelector(".nav__close");
 const nav = document.querySelector(".nav");
+const closeButton = document.querySelector(".nav__close");
+const showBarButton = document.querySelector(".showbar__button");
+const getWidgetsButton = document.querySelector(".nav__button");
 
 function hideBar() {
   return (nav.style.display = "none");
@@ -10,5 +11,12 @@ function showBar() {
   return (nav.style.display = "block");
 }
 
+function showAlertMessage(message) {
+  alert(message);
+}
+
 closeButton.addEventListener("click", () => hideBar());
 showBarButton.addEventListener("click", () => showBar());
+getWidgetsButton.addEventListener("click", () =>
+  showAlertMessage("Hello, world!")
+);
